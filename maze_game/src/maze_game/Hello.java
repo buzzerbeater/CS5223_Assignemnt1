4a4cf78a813ca786a73dea36e72a77955cd47282
Boyan Qi <qiby@garena.com>
2018-09-15 17:47:21 +0800

    UPD: change demo project to adapt to IDE env

---
+++ maze_game/src/maze_game/Hello.java
@@ -0,0 +1,8 @@
+package maze_game;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Hello extends Remote {
+    String sayHello() throws RemoteException;
+}
--- maze_game/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author lixy
- *
- */
-module maze_game {
-}
